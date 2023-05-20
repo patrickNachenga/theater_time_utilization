5fc7e76c3ea859510b6fe68ffc1fd44b426294a9
patrickNachenga <patricknachenga@gmail.com>
2023-05-20 18:27:19 +0300

    adding programme

--- alembic/env.py
+++ alembic/env.py
@@ -5,8 +5,6 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from src.core.config import settings
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -19,18 +17,13 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
-import os
-from src.models import Base
-
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 
 def run_migrations_offline() -> None:
--- src/apis.py
+++ src/apis.py
@@ -1,15 +1,18 @@
 import strawberry
 
 from src.modules.course.apis import CourseQuery, CourseMutation
+from src.modules.groups.apis import GroupQuery, GroupMutation
 from src.modules.program_type.apis import ProgramCategoryQuery, ProgramCategoryMutation
+from src.modules.programmes.apis import ProgrammeQuery, ProgrammeMutation
 from src.modules.staff.apis import StaffQuery, StaffMutation
 from src.modules.students.apis import StudentQuery, StudentMutation
 
+
 @strawberry.type
-class ApiQuery(StudentQuery, StaffQuery, ProgramCategoryQuery, CourseQuery):
+class ApiQuery(StudentQuery, StaffQuery, ProgramCategoryQuery, CourseQuery, GroupQuery, ProgrammeQuery):
     pass
 
 
 @strawberry.type
-class ApiMutation(StudentMutation, StaffMutation, ProgramCategoryMutation, CourseMutation):
+class ApiMutation(StudentMutation, StaffMutation, ProgramCategoryMutation, CourseMutation, GroupMutation, ProgrammeMutation):
     pass
