a96ad67aa4e0017206dbb9945baac9b0e3ea2c2c
patrickNachenga <patricknachenga@gmail.com>
2023-06-06 10:40:36 +0300

    add sr2 rest api

--- src/apis.py
+++ src/apis.py
@@ -9,6 +9,7 @@ from src.modules.program_category.apis import ProgramCategoryQuery, ProgramCateg
 from src.modules.program_course.apis import ProgramCourseQuery, ProgramCourseMutation
 from src.modules.program_semester.apis import ProgramSemesterQuery, ProgramSemesterMutation
 from src.modules.programs.apis import ProgramQuery, ProgramMutation
+from src.modules.sr2_api_calls.apis import Sr2Query
 from src.modules.staff.apis import StaffQuery, StaffMutation
 from src.modules.students.apis import StudentQuery, StudentMutation
 from src.modules.exam_results.apis import ExamResultsQuery, ExamResultsMutation
@@ -23,7 +24,7 @@ from src.modules.program_course_assessment.apis import ProgramCourseAssessmentQu
 class ApiQuery(StudentQuery, StaffQuery, ProgramCategoryQuery, CourseQuery, CourseAllocationQuery,
                CourseCategoryQuery, GroupQuery, ProgramQuery, ExamCatGroupsQuery, ExamCatsQuery, ExamResultsQuery,
                ExamSummaryQuery, AcademicYearQuery, ProgramSemesterQuery, CourseLearnOutcomeQuery, ProgramCourseQuery,
-               ProgramCourseAssessmentQuery):
+               ProgramCourseAssessmentQuery, Sr2Query):
     pass
 
 
--- src/modules/sr2_api_calls/service.py
+++ src/modules/sr2_api_calls/service.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, List
 
 import requests
 
@@ -6,45 +6,48 @@ from src.models import Program
 from src.modules.programs.service import ProgramService
 from src.shared.response import Response
 from src.shared.response_code import ResponseCode
-from src.types import ProgramFeeStructureInput
+from src.types import FeeStructureInput, FeeStructureNode
 
 
 class Sr2ApiCalls(object):
     token = '9454c6efdb94236e618c9a7b1a67138b'
-    site_url = 'http://10.2.1.165/sr2/rest/server.php'
+    site_url = 'http://197.250.34.41:4747/api/v2/'
 
     @staticmethod
-    async def request_fee_structure(inputs: ProgramFeeStructureInput) -> Response[Any]:
+    def request_fee_structure(inputs: FeeStructureInput) -> List[FeeStructureNode] | None:
         try:
             # Verify and get supplied Program uid and get existed program model
-            program = ProgramService(Program).get_program_by_code(inputs.code)
-            if program is None:
-                return Response(
-                    status=False,
-                    code=ResponseCode.FAILURE,
-                    data=None,
-                    message="You have submitted incorrect program details"
-                )
-
+            # program = ProgramService(Program).get_program_by_code(inputs.program_code)
+            # if program is None:
+            #     return None
             # Set the request payload
+            # payload = {
+            #     "program_code": inputs.program_code,
+            #     "year_of_study": inputs.year_of_study,
+            #     "study_level": inputs.study_level,
+            #     "student_status": inputs.student_status,
+            #     "countrycode": inputs.countrycode,
+            # }
             payload = {
-                'token': Sr2ApiCalls.token,
-                'function': 'core_user_create_users',
-                'format': 'json',
-                'data[0][code]': inputs.code,
-                'data[0][study_year]': inputs.study_year,
+                "program_code": "AGC",
+                "year_of_study": 0,
+                "study_level": "Diploma",
+                "student_status": "Unregistered",
+                "countrycode": 255
             }
             # Send the Get request
-            response = requests.get(Sr2ApiCalls.site_url, data=payload)
+            response = requests.get(Sr2ApiCalls.site_url+"/billing/program_fee_structure", data=payload)
             # Check for errors
+            response_data = response.heaer
+            print(response_data)
             if response.status_code == 200:
                 response_data = response.json()
                 print(response_data)
-                return response_data
+                return []
             else:
-                print('HTTP Error:', response.status_code)
+                print('HTTP Error:', response.json())
                 # Decode the response
-                return None
+                return []
         except Exception as e:
             print(e)
-            return None
+            return []
