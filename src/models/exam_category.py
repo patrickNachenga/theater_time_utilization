3ad2c9a4d811166c6fef38bde4adcb8f73047c0c
$olerGenius <soler.deus@sua.ac.tz>
2023-06-11 10:57:20 +0300

    Exam Categories Changes

--- src/models/exam_category.py
+++ src/models/exam_category.py
@@ -4,10 +4,10 @@ from sqlalchemy.orm import relationship
 from src.models import BaseModel
 
 
-class ExamCategories(BaseModel):
+class ExamCategory(BaseModel):
     __tablename__ = "exam_categories"
     code: str = Column(String, nullable=False, unique=False)
     name: str = Column(String, nullable=False, unique=False)
     exam_category_group_id: int = Column(Integer, ForeignKey("exam_category_groups.id"), nullable=False)
 
-    exam_category_group = relationship('ExamCategoryGroup', lazy='subquery', back_populates="exam_categories")
+    exam_category_group = relationship('ExamCategoryGroup', lazy='subquery', back_populates="exam_category")
