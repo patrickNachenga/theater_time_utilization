d8d70f18390f862782c50ad8835c89c4e542054d
patrickNachenga <patricknachenga@gmail.com>
2023-06-14 20:07:28 +0300

    change get REST program endpoint

--- src/api_routes/program_api.py
+++ src/api_routes/program_api.py
@@ -24,24 +24,24 @@ from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 
 
-# @program_router.get(root_path)
-# async def get_program_data(code: str | None = None, uid: str | None = None):
-#     if code:
-#         return await ProgramService.api_get_program_by_code(code=code)
-#     elif uid:
-#         return await ProgramService.api_get_program_by_code(uid=uid)
-#     else:
-#         return await ProgramService.api_get_programs()
+@program_router.get(root_path)
+async def get_program_data(code: str | None = None, uid: str | None = None):
+    if code:
+        return await ProgramService.api_get_program_by_code(code=code)
+    elif uid:
+        return await ProgramService.api_get_program_by_code(uid=uid)
+    else:
+        return await ProgramService.api_get_programs()
 
 
 class ProgramDepartmentInput(BaseModel):
     departments: List[str]
 
 
-@program_router.get("/program")
-async def get_program_data(parm: ProgramCodeInput):
-    print(parm)
-    return await ProgramService.api_get_program_by_code(parm)
+# @program_router.get("/program")
+# async def get_program_data(parm: ProgramCodeInput):
+#     print(parm)
+#     return await ProgramService.api_get_program_by_code(parm)
 
 
 @program_router.get("/programs")
