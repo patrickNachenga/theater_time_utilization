44d09360eff024d041752e0e3600c4b68e404cef
HUSSEIN MKWAZU <husseinmkwazu@gmail.com>
2024-02-04 15:28:15 +0000

    Update program_semester.py

--- src/models/program_semester.py
+++ src/models/program_semester.py
@@ -20,7 +20,7 @@ class ProgramSemester(BaseModel):
     # program = relationship('Program', lazy='subquery')
 
     # ---------------Referenced Columns ---------------------
-    # program_courses = relationship('ProgramCourse', lazy='subquery', back_populates="program_semester")
+    program_courses = relationship('ProgramCourse', lazy='subquery', back_populates="program_semester")
     # semester_registrations = relationship('SemesterRegistration', lazy='subquery', back_populates="semester_program")
 
 
