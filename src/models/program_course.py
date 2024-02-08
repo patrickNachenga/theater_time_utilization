696c4eb81e7e75702e0691cc7f382367ad060837
Hussein Mkwazu <husseinmkwazu@sua.ac.tz>
2024-02-08 13:45:29 +0300

    Update Course Allocation, Program Course

--- src/models/program_course.py
+++ src/models/program_course.py
@@ -32,13 +32,15 @@ class ProgramCourse(BaseModel):
 
     program_course_assessments = relationship('ProgramCourseAssessment', lazy='subquery',
                                               back_populates="program_course")
-    course_allocations = relationship("CourseAllocation", lazy="noload", back_populates="program_course")
+    # course_allocations = relationship("CourseAllocation", lazy="noload", back_populates="program_course")
+    course_allocations = relationship("CourseAllocation", lazy="noload")
 
-    student_course_registrations = relationship("StudentCourseRegistration", lazy="noload",
-                                                back_populates="program_course")
+    # student_course_registrations = relationship("StudentCourseRegistration", lazy="noload",
+    #                                             back_populates="program_course")
 
-    exam_results = relationship("ExamResult", lazy="noload",
-                                back_populates="program_course")
+    student_course_registrations = relationship("StudentCourseRegistration", lazy="noload")
 
-    exam_courseworks = relationship("ExamCoursework", lazy="noload",
-                                    back_populates="program_course")
+    exam_results = relationship("ExamResult", lazy="noload", back_populates="program_course")
+
+    # exam_courseworks = relationship("ExamCoursework", lazy="noload", back_populates="program_course")
+    exam_courseworks = relationship("ExamCoursework", lazy="noload")
