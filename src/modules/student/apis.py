fa38598a6151fd4dc59b562d2e5a14160fec21cc
Hussein Mkwazu <husseinmkwazu@sua.ac.tz>
2024-02-09 13:07:58 +0300

    Arrange content left xls

--- src/modules/student/apis.py
+++ src/modules/student/apis.py
@@ -241,7 +241,7 @@ class StudentMutation:
             # Align the cells to the center
             for col in range(1, 5):
                 cell = worksheet.cell(row=row, column=col)
-                cell.alignment = Alignment(horizontal='center', vertical='center')
+                cell.alignment = Alignment(horizontal='left', vertical='center')
                 cell.font = font
                 cell.border = border
         worksheet.cell(row=1, column=4, value=str(result["program_course"].id))
