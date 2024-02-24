1b023b9669fda0a69bb70762d586332240479114
Hussein Mkwazu <husseinmkwazu@sua.ac.tz>
2024-02-24 15:57:43 +0300

    Add group by

--- src/modules/exam_coursework/service.py
+++ src/modules/exam_coursework/service.py
@@ -80,7 +80,7 @@ class ExamCourseworkService:
                     ExamCoursework.deleted_at.is_(None),
                     latest_exam_result_summary_subquery.c.row_number == 1  # Select only the latest entry
                 ).group_by(
-                    ExamCoursework.student_id,
+                    ExamCoursework.student_uid,
                     ExamCoursework.exam_category_id,
                     ExamCoursework.assessment_number,
                     ExamCoursework.program_course_id
