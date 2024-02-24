4093d13b7c27276ece7563976f1e03b4f3c7c68a
Hussein Mkwazu <husseinmkwazu@sua.ac.tz>
2024-02-24 22:04:21 +0300

    Update ExamCourseWorkNode

--- src/types.py
+++ src/types.py
@@ -1279,7 +1279,7 @@ class ExamCourseWorkNode:
     assessment_number: int
     source: str
     overall_marks: float
-    program_course: ProgramCourseNode
+    program_course: Optional[ProgramCourseNode] = None
     exam_category: ExamCategoryNode
 
 
