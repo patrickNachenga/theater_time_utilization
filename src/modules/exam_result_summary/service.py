8da4c5d9632e8b05403067e08d2256b57b52e046
Hussein Mkwazu <husseinmkwazu@sua.ac.tz>
2024-03-23 10:08:23 +0300

    Update UE exams logs

--- src/modules/exam_result_summary/service.py
+++ src/modules/exam_result_summary/service.py
@@ -69,6 +69,7 @@ class ExamResultSummaryService((CRUDBase[ExamResultSummary, ExamResultSummaryInp
                 query = query.filter(ExamResultSummary.semester == search_criteria.semester)
             if search_criteria.publish_status:
                 query = query.filter(ExamResultSummary.publish_status == search_criteria.publish_status)
+                print('Checking UE: ', search_criteria.student_uid)
             query = query.order_by(ExamResultSummary.registration_number.asc())
             # Execute the final query
             results = query.all()
