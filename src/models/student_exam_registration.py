afa17a60dde5110000ebf9a3b1bb4a8283f8fbab
Hussein Mkwazu <husseinmkwazu@sua.ac.tz>
2024-03-25 21:20:57 +0300

    Update Viewing single student results

--- src/models/student_exam_registration.py
+++ src/models/student_exam_registration.py
@@ -8,9 +8,7 @@ class StudentExamRegistration(BaseModel):
     __tablename__ = 'student_exam_registrations'
 
     # ______________________________Foreign Keys__________________________________________________#
-
     student_course_registration_id: int = Column(Integer, ForeignKey("student_course_registrations.id"), nullable=False)
-
     # _____________________________Relationships___________________________________________________#
 
     # failure type 1 is first sitting, 2 probation, 3 third attempt, 4 retake
