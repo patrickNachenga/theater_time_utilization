2e5f4833e83c8a2f05228c9ea25ecc3c481d1332
patrickNachenga <patricknachenga@gmail.com>
2024-04-30 15:54:14 +0300

    merge changes

--- src/helpers/apscheduler.py
+++ src/helpers/apscheduler.py
@@ -8,7 +8,8 @@ from src.helpers.utils import create_course_to_moodle, create_group_to_moodle, e
 scheduler = BackgroundScheduler()
 
 # add required operation on job
-scheduler.add_job(create_course_to_moodle, 'interval', seconds=33
+scheduler.add_job(create_course_to_moodle, 'interval', seconds=30)
+scheduler.add_job(create_group_to_moodle, 'interval', seconds=40)
 scheduler.add_job(enroll_staff_to_moodle_course, 'interval', seconds=10)
 scheduler.add_job(enroll_student_to_moodle_course, 'interval', seconds=70)
 scheduler.add_job(enroll_student_to_moodle_group, 'interval', seconds=80)
